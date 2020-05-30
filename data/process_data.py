85d9e602a0807cf8d9d878e17ceb4f177b9d5566
christoph1604 <cmf.wagner@t-online.de>
2020-05-30 21:51:55 +0000

    feat: process_data.py created and filled

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,45 @@
+# import libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    messages = messages[~messages.id.duplicated()]
+    
+    categories = pd.read_csv(categories_filepath)
+    categories = categories[~categories.id.duplicated()]
+    
+    df = messages.merge(categories, left_on=["id"], right_on=["id"])
+    return df
 
 def clean_data(df):
-    pass
+    categories = df.categories.str.split(pat=";", expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda str: str[0:-2])
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.slice(start=-1)
+    
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column], downcast="integer")
+        
+    df.drop(["categories"], axis=1, inplace=True)
+    
+    df.reset_index(drop=True, inplace=True)
+    categories.reset_index(drop=True, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    
+    df=df[~df.duplicated()]
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('MessageData', engine, index=False)
 
 
 def main():
