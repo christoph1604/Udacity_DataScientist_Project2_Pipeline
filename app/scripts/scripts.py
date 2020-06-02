00e59b4e61ca6a6b60614a524280029fc335d2e0
christoph1604 <cmf.wagner@t-online.de>
2020-06-03 00:20:19 +0200

    feat: new model and new scripts file

--- app/scripts/scripts.py
+++ app/scripts/scripts.py
@@ -1,10 +1,21 @@
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
 def tokenize(text):
-    tokens = word_tokenize(text)
+    found_urls=re.findall(url_regex, text)
+    for pos in found_urls:
+        text=text.replace(pos, "urlplaceholder")
+    text=re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    text=text.lower()
+    words=nltk.word_tokenize(text)
+    
     lemmatizer = WordNetLemmatizer()
-
     clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
+    for w in words:
+        tok=lemmatizer.lemmatize(w).lower().strip()
+        clean_tokens.append(tok)
+        
     return clean_tokens
